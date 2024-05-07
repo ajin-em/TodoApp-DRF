2350127bc376af41ab1ca5904c1ab41066e692a0
ajin-em <ajinem2@gmail.com>
2024-05-07 11:21:10 +0530

    url modified

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/tasks/', include('tasks.urls')),
 ]
